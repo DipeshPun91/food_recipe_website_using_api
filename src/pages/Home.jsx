cad0966095e627f3808a27853e2f47cd7b0c8959
DipeshPun91 <pundipesh091@gmail.com>
2025-08-04 17:55:30 +0545

    Eight commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Menu from "../component/partials/Menu";
-import Features from "../component/partials/Features";
-import Testimonials from "../component/partials/Testimonials";
+import Features from "../component/home/Features";
+import Testimonials from "../component/home/Testimonials";
 import Cta from "../component/home/Cta";
 import Hero from "../component/home/Hero";
 
